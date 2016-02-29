8961f69dbfb017dc3b0761ed788079b3e6d11f89
Karachentseva <well.karachentseva@gmail.com>
2016-02-29 23:42:29 +0200

    Replacement of the existing method on StringBuilder
    Distinct  method returns distinct elements from a sequence by using a
    comparator for comparing the default values of equality that does not
    satisfy the condition of laboratory work.

--- vis_lab_1/vis_lab_1/Form1.cs
+++ vis_lab_1/vis_lab_1/Form1.cs
@@ -19,9 +19,26 @@ namespace vis_lab_1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String str = textBox1.Text;
-            String str2 = new string(str.Distinct().ToArray());
-            textBox2.Text = str2;
+            StringBuilder str = new StringBuilder(textBox1.Text);
+
+            for (int i = 1; i <= str.Length - 1; i++)
+
+            {
+
+                if (str[i] == str[i - 1])
+
+                {
+
+                    str.Remove(i, 1);
+
+                    i--;
+
+                }
+
+            }
+            
+            textBox2.Text = str.ToString();
+          
         }
     }
 }
