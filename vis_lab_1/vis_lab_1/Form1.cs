3fb7e3dd4f51c79cfe41d42b45af86d52307eafb
CrossNick <nagatopeih@yandex.ru>
2016-03-01 08:07:08 +0200

    Final version of code and form

--- vis_lab_1/vis_lab_1/Form1.cs
+++ vis_lab_1/vis_lab_1/Form1.cs
@@ -20,25 +20,12 @@ namespace vis_lab_1
         private void button1_Click(object sender, EventArgs e)
         {
             StringBuilder str = new StringBuilder(textBox1.Text);
-
             for (int i = 1; i <= str.Length - 1; i++)
-
-            {
-
                 if (str[i] == str[i - 1])
-
                 {
-
-                    str.Remove(i, 1);
-
-                    i--;
-
+                    str.Remove(i--, 1);
                 }
-
-            }
-            
             textBox2.Text = str.ToString();
-          
         }
     }
 }
